290ddb0202dea679b437696613544c75b10025a7
Asim Mittal <asim.mittal@gmail.com>
2015-05-17 21:44:20 -0700

    modifications made

--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,31 @@
 var showcaseUrl = "showcase.txt";
 
-$(function(){
+$(function(){    
     $.get(showcaseUrl, function(response){
         var showcaseItems = ($.parseJSON(response)).items;        
         var param = window.location.search;
         var index = parseInt(param.substring(param.indexOf("=") + 1));        
         var gameItem = showcaseItems[index];
         
+        console.log("--> " + gameItem.title);
+        
+        $.ajax({
+            type: "GET",
+            url: gameItem.help,
+            async: false,
+            jsonpCallback: 'info',
+            contentType: "application/json",
+            dataType: 'jsonp',
+            success: function(json){
+                $("#help").html(json[0]);
+            },
+            error: function(){
+                $("#help").html("No instructions found");
+            }
+        });
+        
         $("#gameTitle").html(gameItem.title);
         $("#credits").html(gameItem.credits);
-        $("#help").html(gameItem.help);
         $("#iframe").attr("src", gameItem.url);
     });    
 });
